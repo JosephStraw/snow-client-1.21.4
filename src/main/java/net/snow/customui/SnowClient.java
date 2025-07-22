ccd7a7737d529c3d1fe561ff3160846c1b182ad7
Joe <cwsnow@protonmail.com>
2025-07-22 20:32:36 +0100

    attempt02

---
+++ src/main/java/net/snow/customui/SnowClient.java
@@ -0,0 +1,16 @@
+package net.snow.customui;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SnowClient implements ModInitializer {
+	public static final String MOD_ID = "snowclient";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/snow/customui/SnowClientJClient.java
@@ -0,0 +1,10 @@
+package net.snow.customui;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SnowClientJClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
